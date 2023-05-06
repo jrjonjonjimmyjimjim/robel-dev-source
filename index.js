df34feba3b33e50d687184b6ffbc6588515295b9
Jonathon Robel <jonrrobel@gmail.com>
2023-05-06 16:34:41 -0600

    Begin experimenting with canvas

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ var Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
     Bodies = Matter.Bodies,
+    MouseConstraint = Matter.MouseConstraint,
+    Mouse = Matter.Mouse,
     Composite = Matter.Composite;
 
-// create an engine
+// create an engine. the engine is what is actually calculating the physics.
 var engine = Engine.create();
 
-// create a renderer
+// create a renderer. The renderer visualizes what the engine is doing.
 var render = Render.create({
     element: document.body,
     engine: engine
@@ -25,8 +27,25 @@ Composite.add(engine.world, [boxA, boxB, ground]);
 // run the renderer
 Render.run(render);
 
-// create runner
+// create runner. The runner does general-purpose physics updates for testing.
 var runner = Runner.create();
 
 // run the engine
 Runner.run(runner, engine);
+
+// add mouse control
+var mouse = Mouse.create(render.canvas),
+mouseConstraint = MouseConstraint.create(engine, {
+    mouse: mouse,
+    constraint: {
+        stiffness: 0.2,
+        render: {
+            visible: false
+        }
+    }
+});
+
+Composite.add(engine.world, mouseConstraint);
+
+// keep the mouse in sync with rendering
+render.mouse = mouse;
